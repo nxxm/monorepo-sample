98ea44cc34ddc5d71da9eb685ef1f8c9fb6e9b62
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2021-10-01 19:19:05 +0200

    :mag: monorepo sample

---
+++ app/main.cpp
@@ -0,0 +1,7 @@
+
+#include <foo/foo.hpp>
+
+int main(int argc, char** argv) {
+    foo::print();
+    return 0;
+}
---
+++ libFoo/foo/foo.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+namespace foo {
+    void print() {
+        std::cout << "FOO" << std::endl;
+    }
+}
